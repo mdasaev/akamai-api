d92a85df26a72ca2db26b9931c3fc35b42cacdec
Marat Dasaev <marat.dasaev@gmail.com>
2023-08-15 09:37:37 +0200

    minor fixes

--- hosts.go
+++ hosts.go
@@ -43,7 +43,7 @@ func main() {
 	//Check for new hostnames
 
 	//result := ListSelected(AkamaiHost, configID, version, policyID)
-	configHostnames := ListSelectableOnConfig(AkamaiHost, configID, version)
+	configHostnames := ListSelectedOnPolicy(AkamaiHost, configID, version, policyID)
 	golog.Info("Configuration hostnames: " + string(configHostnames))
 
 	policy := new(selectableHostnames)
